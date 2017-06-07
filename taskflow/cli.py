f1cfb61795c4a9648ded280faf62ae57ab77f6f9
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-06-07 14:28:02 -0400

    Add worker id

--- taskflow/cli.py
+++ taskflow/cli.py
@@ -3,7 +3,10 @@ import time
 import logging
 from datetime import datetime
 import json
+import os
+import socket
 
+import requests
 import click
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -17,6 +20,32 @@ def get_logging():
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
+
+def get_worker_id():
+    worker_components = []
+
+    ## AWS
+    response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+    if response.status_code == 200:
+        worker_components.append(response.text)
+
+    ## ECS (AWS Batch uses ECS as well)
+    response = requests.get('http://172.17.0.1:51678/v1/tasks')
+    if response.status_code == 200:
+        tasks = response.json()['Tasks']
+        short_docker_id = os.getenv('HOSTNAME', None) ## ECS marks the short docker id as the HOSTNAME
+        if short_docker_id != None:
+            matched = list(filter(
+                lambda ecs_task: ecs_task['Containers'][0]['DockerId'][0:12] == short_docker_id,
+                tasks))
+            if len(matched) > 0:
+                worker_components.append(matched[0]['Containers'][0]['Arn'])
+
+    ## fallback to IP
+    if len(worker_components) == 0:
+        return socket.gethostbyname(socket.gethostname())
+    else:
+        return worker_components.join('-')
 
 @click.group()
 @click.option('--taskflow')
@@ -91,7 +120,8 @@ def pull_worker(ctx, sql_alchemy_connection, num_runs, dry_run, now_override, sl
     if task_names != None:
         task_names = task_names.split(',')
 
-    worker_id = get_worker_id()
+    if worker_id == None:
+        worker_id = get_worker_id()
 
     for n in range(0, num_runs):
         session = Session()
@@ -118,7 +148,8 @@ def run_task(ctx, task_instance_id, sql_alchemy_connection, worker_id):
     engine = create_engine(connection_string)
     Session = sessionmaker(bind=engine)
 
-    worker_id = get_worker_id()
+    if worker_id == None:
+        worker_id = get_worker_id()
 
     session = Session()
 
