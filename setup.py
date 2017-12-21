ceabe1785cfcb99e9152c6f23ee414026fe6b8f8
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-12-21 16:15:44 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='taskflow',
-    version='0.2.6',
+    version='0.2.7',
     packages=find_packages(),
     install_requires=[
         'alembic==0.9.6',
